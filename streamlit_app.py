9a67e02c0c3ebb7f9094b67de6bb0717b9e7a92e
santanupatra2023 <127126930+santanupatra2023@users.noreply.github.com>
2023-03-06 10:45:44 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 
-streamlit.title('Tia's Amazing Restaurant')
+streamlit.title('Tias Amazing Restaurant')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal 🥣')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
- ##🥗 🐔 🥑🍞
+ 🥗 🐔 🥑🍞
