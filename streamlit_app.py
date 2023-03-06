f760893f3fa6f5eb00c9fd5109511f4c11bd8982
santanupatra2023 <127126930+santanupatra2023@users.noreply.github.com>
2023-03-06 11:47:20 -0600

    Update streamlit_app.py
    Pandas code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,3 @@
-import streamlit
-
-streamlit.title('Tias Amazing Restaurant 🥑')
-streamlit.header('Breakfast Menu 🍞')
-streamlit.text('Omega 3 & Blueberry Oatmeal 🥣')
-streamlit.text('Kale, Spinach & Rocket Smoothie🥗')
-streamlit.text('Hard-Boiled Free-Range Egg🐔')
+import pandas
+my_fruit_list = pandas.read_cvs("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe (my_fruit_list)
