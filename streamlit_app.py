5f6c7d64c2c711656a984acbcd1dfa7b26b98196
santanupatra2023 <127126930+santanupatra2023@users.noreply.github.com>
2023-03-06 10:45:23 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal 🥣')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
- 🥗 🐔 🥑🍞
+ ##🥗 🐔 🥑🍞
